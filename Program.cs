3dabb005e6a2adec97dcb33a1f98e38802fa9773
Valentin <nitnelav6691@gmail.cov>
2024-01-23 08:29:09 +0300

    Введена функция

--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
 ﻿string[] array = {"if", "console", "for", "12345", "000"};
 Console.WriteLine($"[{String.Join(',', array)}]");
+ArrayConversion(array);
 
-    for (int i = 0; i < array.Length; i++)
+void ArrayConversion(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
     {
-    int length = array[i].Length;
+    int length = arr[i].Length;
         if(length < 4)
         {
-            Console.Write(array[i] + " ");
+            Console.Write(arr[i] + ", ");
         }
         else
         {
-            array[i] = "";
+            arr[i] = "";
+        }
     }
 }
--- obj/Debug/net8.0/Work.AssemblyInfo.cs
+++ obj/Debug/net8.0/Work.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Work")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eaac085b68d54ceaee57aaf9aa9fe4ee9ccaa57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f85c72189b64bfdae92c6459182fbcd51bd9fe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Work")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Work")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
